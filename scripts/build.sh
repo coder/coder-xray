d1379d1cfc8ded37b8ab32ce3e19a0106a111f7d
Jon Ayers <jon@coder.com>
2024-02-01 00:54:17 +0000

    fix: use current directory in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ cd $(dirname "${BASH_SOURCE[0]}")
 set -euxo pipefail
 
 CGO_ENABLED=0 go build -ldflags "-s -w" -o ./coder-xray ../
-docker build -t coder-xray:latest 
+docker build -t coder-xray:latest .
